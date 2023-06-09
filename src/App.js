ae372af1c2d7377bab66da997bbe678fafbe8bfa
svanka024 <1196688@student.roc-nijmegen.nl>
2023-06-09 10:35:36 +0200

    comiit

--- src/App.js
+++ src/App.js
@@ -31,5 +31,13 @@ export default App;
 
 console.log(
   const [getTaskList, setTaskList] = useState([]);
-  const jobCollectionRef = collection( )
+  const jobCollectionRef = collection(db, "tasks");
+
+  useEffect(()=>{
+    const getTasks = async () => {
+      const data = await getDocs(jobCollectionRef);
+      setTaskList(data.docs.map((docs) => ({...doc.data(), id: doc.id})));
+    }
+    getTasks();
+  },[])
 );
