5a8c830e408addcd76b841e64d884e7cb6eeedce
jaspervanuden <jaspervanuden03@gmail.com>
2023-06-09 10:34:22 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,9 @@ const firebaseConfig = {
   projectId: "festihub-4f00c",
   storageBucket: "festihub-4f00c.appspot.com",
   messagingSenderId: "726140180757",
-  appId: "1:726140180757:web:09f12177455131b1a3f11c"
+  appId: "1:726140180757:web:09f12177455131b1a3f11c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
